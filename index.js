e42f0c8ecaace1525b502d07625c37b625435518
harshal patel <41120010+harshalpat@users.noreply.github.com>
2021-03-21 18:49:29 +0100

    added comments

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-var rect = {
-    perimeter: (s,y) => (2*s*y),
-    area: (s,y) => (s*y)
-};
+var rect = require('./Rectangle');
 
 function solveRectangle (l,b) {
     console.log("solving for rectangel with l = "+ l + " and b = " + b);
 
+    // when error occurs
     if(l <= 0 || b <= 0) {
         console.log("rectangle dimentions should be greater than zero : l = "+ l + " and b = " +b);
     
     }
+    //this is the else block when there is no error
     else {
         console.log("The area of rectangle is " + rect.area(l,b));
         console.log("The perimeter of the rectangle is "+ rect.perimeter(l,b));
